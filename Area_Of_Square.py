056b4eddf0865550dfc059fe8acf229ef14e3c0a
SahilAliAkbar <114466921+SahilAliAkbar@users.noreply.github.com>
2023-08-27 14:14:14 +0530

    Add files via upload

---
+++ Area_Of_Square.py
@@ -0,0 +1,3 @@
+side = int(input("Enter the side:"))
+Area = side*side
+print("Area of the square="+str(Area))
---
+++ Perimeter_Of_Square.py
@@ -0,0 +1,4 @@
+print("Enter the Side Length of Square: ")
+s = int(input())
+p = 4*s
+print("\nPerimeter = ", p)
