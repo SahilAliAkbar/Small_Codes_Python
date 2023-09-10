a2ca976c6fb45d2dacb7537953f098273230eebb
SahilAliAkbar <114466921+SahilAliAkbar@users.noreply.github.com>
2023-09-10 11:04:53 +0530

    Add files via upload

---
+++ Print_N_terms_in_AP.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the value of a: "))
+d = int(input("Enter the value of d: "))
+n = int(input("Enter the value of n: "))
+for i in range(1,n+1):
+    t_n = a + (i-1)*d
+    print(t_n)
