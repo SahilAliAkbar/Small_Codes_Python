b17ccdcf60b416ca1cad97ff875f3847b0f24f16
SahilAliAkbar <114466921+SahilAliAkbar@users.noreply.github.com>
2023-09-18 08:49:21 +0530

    Add files via upload

---
+++ Print_Prime_Numbers_1-100.py
@@ -0,0 +1,8 @@
+for Number in range (1, 101):
+    count = 0
+    for i in range(2, (Number//2 + 1)):
+        if(Number % i == 0):
+            count = count + 1
+            break
+    if (count == 0 and Number != 1):
+        print(" %d" %Number, end = '  ')
